b325edd8dd7e32b184f81327de6bb4d62de5e44e
FRANTSSS <francev02@inbox.ru>
2023-05-26 01:08:29 +0600

    BugFix

--- app/public/script/ApplyHandler.js
+++ app/public/script/ApplyHandler.js
@@ -1,4 +1,4 @@
-import {get_events_month} from "./requests.js"
-
+// import {get_events_month} from "./requests.js"
+//
 
 
--- app/public/script/requests.js
+++ app/public/script/requests.js
@@ -30,23 +30,19 @@ function view_get_tag_theme(httpRequest) {
 function get_checked_settings(){
     let checked_tag = []
     let dom = document.getElementById("page__tag_")
-    for(let i= 0; i < dom.length; i++){
-        if(dom[i].children[0].checked){
-            console.log("+")
+    // console.log(dom.children.length);
+    for(let i= 0; i < dom.children.length; i++){
+        // console.log(dom.children[0]);
+        if(dom.children[i].children[0].checked){
+            checked_tag.push(dom.children[i].children[1].textContent);
+            // console.log(dom.children[i].children[1].textContent)
         }
     }
+    console.log(checked_tag);
 }
 
 function view_events_month(httpRequest){
-
-}
-
-export function get_events_month(){
-    let httpRequest = create_request();
-    httpRequest.onreadystatechange = function() { view_events_month(httpRequest); };
-    httpRequest.open("POST", "http://localhost:8040/app/calendar.php", true);
-    httpRequest.setRequestHeader("Content-Type", "application/json;charset=utf-8");
-    httpRequest.send(JSON.stringify({"hren": "yes"}));
+    get_checked_settings();
 }
 
 export function get_group_tag_theme(){
@@ -55,4 +51,12 @@ export function get_group_tag_theme(){
     httpRequest.open("GET", "http://localhost:8040/app/settings.php", true);
     httpRequest.setRequestHeader("Content-Type", "application/json;charset=utf-8");
     httpRequest.send();
+}
+
+export function get_events_month(){
+    let httpRequest = create_request();
+    httpRequest.onreadystatechange = function() { view_events_month(httpRequest); };
+    httpRequest.open("POST", "http://localhost:8040/app/calendar.php", true);
+    httpRequest.setRequestHeader("Content-Type", "application/json;charset=utf-8");
+    httpRequest.send(JSON.stringify({"hren": "yes"}));
 }
